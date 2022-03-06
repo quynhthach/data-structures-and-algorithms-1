a08e8c79471580009262b84e7f6b876b19d6bbbc
Quynh Thach <thachdangdiemquynh@gmail.com>
2022-03-06 15:56:15 -0600

    set up game

--- stacks/towers-of-hanoi.py
+++ stacks/towers-of-hanoi.py
@@ -11,3 +11,15 @@ right_stack = Stack("Right")
 stacks.append(left_stack)
 stacks.append(middle_stack)
 stacks.append(right_stack)
+
+#set up the game
+int = int(input("\nHow many disks do you want to play with?\n"))
+num_disks = int
+
+while num_disks < 3:
+  num_disks = int(input("Enter a number greater than or equal to 3\n"))
+  for i in range(num_disks, 0, -1):
+    left_stack.push(i)
+
+num_optimal_moves = 2 ** num_disks - 1
+print("\nThe fastest you can solve this game is in {0} moves".format(num_optimal_moves))
