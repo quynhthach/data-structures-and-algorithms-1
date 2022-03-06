88ef2077d09d84136af3922c957741c499e68b91
Quynh Thach <thachdangdiemquynh@gmail.com>
2022-03-06 15:54:17 -0600

    create game stacks

---
+++ stacks/towers-of-hanoi.py
@@ -0,0 +1,13 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+stacks = []
+left_stack = Stack("Left")
+middle_stack = Stack("Middle")
+right_stack = Stack("Right")
+
+
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
